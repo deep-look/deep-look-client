369223e10eb841be26fd3df9c65bbba9d6d1bce3
hamo-o <yheel369@gmail.com>
2023-04-26 09:39:23 +0900

    feat: add comment card

---
+++ src/components/common/card/comment/index.tsx
@@ -0,0 +1,82 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { typo } from '@deeplook/styles/theme';
+
+export interface CommentCardProps {
+  name: string;
+  comment: string;
+  img?: string;
+}
+
+export const CommentCard = ({ name, comment, img }: CommentCardProps) => {
+  return (
+    <CommentCardWrapper>
+      <CommentContent>
+        <ImgWrapper>
+          <Img />
+        </ImgWrapper>
+        <Text>
+          <Name css={typo.Subtitle1}>{name}</Name>
+          <Comment css={typo.Body1}>{comment}</Comment>
+        </Text>
+      </CommentContent>
+    </CommentCardWrapper>
+  );
+};
+
+const CommentCardWrapper = styled.div`
+  width: 84%;
+
+  border: 2px solid transparent;
+  border-radius: 0.5rem;
+  background-image: linear-gradient(
+      ${({ theme }) => theme.palette.Black},
+      ${({ theme }) => theme.palette.Black}
+    ),
+    ${({ theme }) => theme.palette.Line};
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+`;
+
+const CommentContent = styled.div`
+  padding: 1.2rem 0 1.2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const ImgWrapper = styled.div`
+  width: 64px;
+  height: 64px;
+
+  border-radius: 50%;
+  overflow: hidden;
+`;
+
+const Img = styled.div`
+  width: 100%;
+  height: 100%;
+
+  background: ${({ theme }) => theme.palette.Gray2};
+  object-fit: cover;
+`;
+
+const Text = styled.div`
+  width: 68%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+`;
+
+const Name = styled.div`
+  width: 100%;
+  color: ${({ theme }) => theme.palette.Yellow2};
+`;
+const Comment = styled.div`
+  width: 100%;
+  color: ${({ theme }) => theme.palette.White};
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import { Navigation } from '@deeplook/components/common/navigation';
+import { CommentCard } from '@deeplook/components/common/card/comment';
+import styled from '@emotion/styled';
 
 export default function Home() {
   return (
@@ -7,10 +9,24 @@ export default function Home() {
       <Head>
         <title>DEEPLOOK</title>
       </Head>
-      <div>
+      <Container>
         <Navigation title="로그인" />
+        <CommentCard
+          name="닉네임"
+          comment="우와 신기해요 어쩌구저쩌ㅁㄴ아럼 ㅏㅊㄹ날먼알ㄹㄴㅇㄹㅁㄴ아"
+        />
         랜딩
-      </div>
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background: ${({ theme }) => theme.palette.Black};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
