2b2bf5abcd6d2d5772059e6984b6e115cbbf94d3
kongnayeon <seoultownroad@naver.com>
2023-05-10 16:41:45 +0900

    fix: test page css

--- src/components/test/index.tsx
+++ src/components/test/index.tsx
@@ -31,38 +31,50 @@ const Test = () => {
     return (
         <div>
             <Navigation title="테스트 하기" />
-            <Flex direction="column">
-                <Space height={104} />
-                <Space height={48} />
-                <Text typo="Heading" color="Yellow">
-                    정면으로 나온 사진을
-                </Text>
-                <Text typo="Heading" color="Yellow">
-                    업로드 해 보세요.
-                </Text>
-                <Space height={24} />
-                <Text typo="Subtitle1" color="White">
-                    최종 학습된 AI 모델을 사용하므로
-                </Text>
-                <Text typo="Subtitle1" color="White">
-                    사진은 절대 저장되지 않습니다.
-                </Text>
-                <Space height={48} />
-                <DropZone
-                    uploadFileHandler={uploadHandler}
-                    fileTypeErrorHandler={() => {
-                        console.log('file type err');
-                    }}
-                    fileNumErrorHandler={() => {
-                        console.log('file num err');
-                    }}
-                />
-                <Space height={144} />
-                <Button onClick={() => router.push('/result')} text="테스트 결과 보기" color="White" varient="dark" />
-                <Space height={60} />
-            </Flex>
+            <StyledFlex direction="column" justify="space-between" gap={84}>
+                <Flex direction="column">
+                    <Space height={104} />
+                    <Space height={48} />
+                    <Text typo="Heading" color="Yellow">
+                        정면으로 나온 사진을
+                    </Text>
+                    <Text typo="Heading" color="Yellow">
+                        업로드 해 보세요.
+                    </Text>
+                    <Space height={24} />
+                    <Text typo="Subtitle1" color="White">
+                        최종 학습된 AI 모델을 사용하므로
+                    </Text>
+                    <Text typo="Subtitle1" color="White">
+                        사진은 절대 저장되지 않습니다.
+                    </Text>
+                    <Space height={48} />
+                    <DropZone
+                        uploadFileHandler={uploadHandler}
+                        fileTypeErrorHandler={() => {
+                            console.log('file type err');
+                        }}
+                        fileNumErrorHandler={() => {
+                            console.log('file num err');
+                        }}
+                    />
+                </Flex>
+                <div>
+                    <Button
+                        onClick={() => router.push('/result')}
+                        text="테스트 결과 보기"
+                        color="White"
+                        varient="dark"
+                    />
+                    <Space height={60} />
+                </div>
+            </StyledFlex>
         </div>
     );
 };
 
 export default Test;
+
+const StyledFlex = styled(Flex)`
+    height: 100vh;
+`;
