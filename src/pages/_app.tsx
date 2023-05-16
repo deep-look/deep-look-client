fd55f97586484d12dc4a4fee648219a6abda2d85
hamo-o <yheel369@gmail.com>
2023-05-16 17:44:27 +0900

    feat: add animatedcursor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,15 @@ import { Global } from '@emotion/react';
 import reset from '@deeplook/styles/reset';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@deeplook/styles/theme';
+import dynamic from 'next/dynamic';
+
+interface CurosrProps {
+  color: string;
+}
+
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </ThemeProvider>
       </Layout>
+      <AnimatedCursor />
     </>
   );
 }
