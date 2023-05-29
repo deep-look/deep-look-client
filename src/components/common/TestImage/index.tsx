b8662b87f565ba01bf55791011179866d9813534
kongnayeon <seoultownroad@naver.com>
2023-05-29 17:41:01 +0900

    fix: resolve conflict

--- src/components/common/TestImage/index.tsx
+++ src/components/common/TestImage/index.tsx
@@ -15,15 +15,7 @@ export const TestImage = ({ value, setValue, ...props }: TestImageProps) => {
     const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             console.log(e.target.files);
-<<<<<<< HEAD
-<<<<<<< HEAD
-            const data = await encodeFileToBase64(e.target.files[0], setValue);
-=======
             const data = await encodeFileToBase64(e.target.files[0]);
->>>>>>> 40d7a17 (fix: TestImage 컴포넌트 수정)
-=======
-            const data = await encodeFileToBase64(e.target.files[0]);
->>>>>>> main
             setValue(data);
         }
     };
