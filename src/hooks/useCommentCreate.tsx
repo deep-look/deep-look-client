43ffcdcaa38db46aa2ac2f749966aecdc3208f66
hamo-o <yheel369@gmail.com>
2023-05-31 12:59:25 +0900

    feat: add useCommentCreate hook

---
+++ src/hooks/useCommentCreate.tsx
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import authAPI from '../apis/auth';
+
+export const useCommentCreate = () => {
+  const queryClient = useQueryClient();
+
+  const createCommentMutation = useMutation(authAPI.COMMENT_CREATE, {
+    onSuccess: async (data: any) => {
+      alert('댓글 작성에 성공했어요!');
+    },
+    onError: (error: any) => {
+      alert('댓글 작성이 어려워요. 다시 시도해 주세요!');
+    },
+  });
+
+  return {
+    createCommentMutation,
+  };
+};
