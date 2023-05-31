6c12a7b271372849a575330fd81d698e11714b77
hamo-o <yheel369@gmail.com>
2023-05-31 15:43:14 +0900

    fix: base64 전송 수정

--- src/apis/predict.tsx
+++ src/apis/predict.tsx
@@ -1,29 +1,30 @@
 import { client } from './client';
 
 export const predictAPI = {
-    POST_PREDICT: async (image: string, accessToken: string): Promise<any> => {
-        const response = await client.post(
-            `/predict`,
-            {
-                image: image,
-            },
-            {
-                headers: {
-                    JWT: `${accessToken}`,
-                },
-            }
-        );
+  POST_PREDICT: async (image: string, accessToken: string): Promise<any> => {
+    console.log(image);
 
-        return response.data;
-    },
-    GET_PREDICT: async (userId: number, accessToken: string): Promise<any> => {
-        const response = await client.get('/predict', {
-            headers: {
-                JWT: `${accessToken}`,
-            },
-        });
-        return response.data;
-    },
+    const base64Data = image.split(',')[1];
+    const formData = new FormData();
+    formData.append('image', base64Data);
+    const response = await client.post(`/predict`, formData, {
+      headers: {
+        JWT: `${accessToken}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    console.log(response);
+
+    return response.data;
+  },
+  GET_PREDICT: async (userId: number, accessToken: string): Promise<any> => {
+    const response = await client.get('/predict', {
+      headers: {
+        JWT: `${accessToken}`,
+      },
+    });
+    return response.data;
+  },
 };
 
 export default predictAPI;
