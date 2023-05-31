417860a667f4c3252c57ec7ecb0a3a8d8c640989
hamo-o <yheel369@gmail.com>
2023-05-31 12:59:39 +0900

    feat: add useCommentList hook

---
+++ src/hooks/useCommentList.tsx
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+
+import authAPI from '@deeplook/apis/auth';
+
+const useCommentList = () => {
+  const { data } = useQuery<any>(['comments'], authAPI.COMMENT_LIST, {
+    onError: () => {
+      console.error('댓글 작성이 어려워요. 다시 시도해 주세요!');
+    },
+  });
+  return data;
+};
+
+export default useCommentList;
