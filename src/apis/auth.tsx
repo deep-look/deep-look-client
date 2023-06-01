8e2e97c7b98320a051802737690dd92c12c8cd87
kongnayeon <seoultownroad@naver.com>
2023-06-01 14:53:24 +0900

    feat: validate api 추가

--- src/apis/auth.tsx
+++ src/apis/auth.tsx
@@ -4,41 +4,22 @@ import { getCookie } from '@deeplook/utils/cookie';
 const accessToken = getCookie();
 
 export const authAPI = {
-  KAKAO_VALIDATION: async (userId: number): Promise<any> => {
-    const response = await client.post('/token/issue', {
-      userId: userId,
-    });
+    KAKAO_VALIDATION: async (userId: number): Promise<any> => {
+        const response = await client.post('/token/issue', {
+            userId: userId,
+        });
 
-    return response.data;
-  },
+        return response.data;
+    },
+    TOKEN_VALIDATION: async (accessToken: string): Promise<any> => {
+        const response = await client.get('/token/validate', {
+            headers: {
+                JWT: `${accessToken}`,
+            },
+        });
 
-  TEST: async (userId: number, accessToken: string): Promise<any> => {
-    const response = await client.get('/token/test', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    return response.data;
-  },
-
-  COMMENT_LIST: async (): Promise<any> => {
-    const response = await client.get('/comment', {
-      headers: {
-        JWT: `${accessToken}`,
-      },
-    });
-    return response.data;
-  },
-
-  COMMENT_CREATE: async (value: string): Promise<any> => {
-    const response = await client.post('/comment', {
-      headers: {
-        JWT: `${accessToken}`,
-      },
-      body: value,
-    });
-    return response.data;
-  },
+        return response.data;
+    },
 };
 
 export default authAPI;
